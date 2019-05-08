dec0524b8f0596e0600e78fa93255167e330ddf6
MayuriLakshmi <durga.mayuri74@gmail.com>
2019-05-08 23:36:53 +0530

    a string added

--- E2EProject1/src/main/java/Academy/E2EProject1/App.java
+++ E2EProject1/src/main/java/Academy/E2EProject1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String s=null;
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         
